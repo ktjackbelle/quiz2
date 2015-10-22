4065e7e5edc92c955130af58e77f21248005de73
ktjackbelle <KatieAPhillips@Katies-Air.ad.uvu.edu>
2015-10-22 08:34:32 -0600

    finished quiz

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function arraySum(arr) {
+  var total = 0;
 
+  for(var i = 0; i < arr.length; i++){
+    if (typeof arr[i] === 'number'){
+      total += arr[i];
+    }else {
+      total += arraySum(arr[i]);
+    }
+  }
+
+  return total;
 }
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function longestString(arr) {
+  var longest;
 
+for (var i = 0; i< arr.length; i++) {
+var isString = typeof arr[i] === 'string';
+var isLonger = !longest || arr[i].length > longest.length;
+  if (isString && isLonger){
+    longest = arr[i];
+  }
 }
 
+return longest;
+}
 module.exports = longestString;
